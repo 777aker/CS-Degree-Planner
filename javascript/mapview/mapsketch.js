c44f32eb1bf64a2509a219a72b3f0801c116fe53
Kelley Kelley <mike1555@colorado.edu>
2022-07-29 14:57:25 -0600

    edit menu hover

--- javascript/mapview/mapsketch.js
+++ javascript/mapview/mapsketch.js
@@ -6,14 +6,60 @@ function setup() {
   background(220);
   // set framerate to 30 anything more is a waste of power
   frameRate(30);
-  // create add course button
-  button = createButton('Add Course');
-  button.position(width-100, height-50);
-  button.mousePressed(addCourse);
-  button.id('addcoursebutton');
+  // set up the edit menu
+  editMenuSetUp();
 }
 
+function editMenuSetUp() {
+  // create add course button
+  button = createButton('Add Course');
+  button.mousePressed(addCourse);
+  button.class("editbuttons");
+  button.parent("#dropdown-content");
+  // create add node button
+  button = createButton('Add Node');
+  button.mousePressed(addNode);
+  button.class("editbuttons");
+  button.parent("#dropdown-content");
+  // create draw path button
+  button = createButton('Draw Path');
+  button.mousePressed(drawPath);
+  button.class("editbuttons");
+  button.parent("#dropdown-content");
+  // create delete button
+  button = createButton('Delete Mode');
+  button.mousePressed(deleteMode);
+  button.class("editbuttons");
+  button.parent("#dropdown-content");
+  // create move button
+  button = createButton('Edit Positions');
+  button.mousePressed(editPositions);
+  button.class("editbuttons");
+  button.parent("#dropdown-content");
+}
+
+// function called when user presses add course button
 function addCourse() {
+
+}
+
+// function called when user presses add node button
+function addNode() {
+
+}
+
+// function called when user presses draw path button
+function drawPath() {
+
+}
+
+// function called when user presses delete mode button
+function deleteMode() {
+
+}
+
+// function called when user presses edit positions button
+function editPositions() {
 
 }
 
@@ -24,7 +70,9 @@ function draw() {
 
 // if the window is resized this function is called
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth-20, windowHeight-20);
+  // set background color
+  background(220);
 }
 
 //  key typed event not for special keys use keyPressed for those
