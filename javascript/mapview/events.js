b301974587df0429e60cc42923aac9d71f61ef3d
Kelley Kelley <mike1555@colorado.edu>
2022-10-06 12:52:32 -0600

    Update events.js

--- javascript/mapview/events.js
+++ javascript/mapview/events.js
@@ -49,6 +49,7 @@ function keyTyped() {
   }
   if(key === 'z') {
     zoom = 1;
+    print('zoom?');
   }
   // I needed a way in drawing mode to see what was going on when debugging
   // (ironic for a drawing mode)
