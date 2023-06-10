54d907a2cba852e62cdc385f93fff3167ecdec56
Kelley Kelley <mike1555@colorado.edu>
2023-06-10 12:00:55 -0600

    removed old planner

--- javascript/planner/setup.js
+++
@@ -1,8 +0,0 @@
-// call two functions that set up some stuff
-function setup() {
-  setupYears();
-  setupDegreeSelector();
-  setupTrash();
-}
-// this is the fun area in the middle of our screen
-const activearea = document.querySelector("#active-area");
