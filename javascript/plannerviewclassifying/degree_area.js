d027f78fa547d2cd0ddc6c0d7f2d5fb98b9cb9b9
Kelley Kelley <mike1555@colorado.edu>
2023-06-21 12:18:12 -0600

    requirement class WIP

--- javascript/plannerviewclassifying/degree_area.js
+++ javascript/plannerviewclassifying/degree_area.js
@@ -1,16 +1,24 @@
+// requirement list
+let requirements;
 // populate degree area with buttons and such
 const degreeSelected = document.querySelector('#degree-selected');
 const degreeArea = document.querySelector('#degree-area');
+const requirementsCompleted = document.querySelector('#requirements-completed');
 function populateDegreeArea() {
   // delete previous data
   degreeArea.innerHTML = '';
   // add degree name
   degreeSelected.innerHTML = degreeName + ' Requirements';
   degreeArea.appendChild(degreeSelected);
-  //TODO: degreeArea.appendChild(requirementsCompleted);
+  degreeArea.appendChild(requirementsCompleted);
 
   // populate each requirement
-  //Object.keys(degreeJSON.requirements).forEach(key => createRequirement(key));
+  for(let requirement in degreeJSON.requirements) {
+    degreeJSON.requirements[requirement] = Object.assign(
+      new Requirement(requirement, degreeJSON.requirements[requirement].courses),
+      degreeJSON.requirements[requirement]
+    );
+  }
 
   //checkRequirements();
 }
---
+++ javascript/plannerviewclassifying/requirement.js
@@ -0,0 +1,32 @@
+// requirement class
+class Requirement {
+  constructor(requirement, courses) {
+    // add the requirement to the degree area
+    let reqBtn = createButton(requirement.replace(/_/g, ' '));
+    reqBtn.parent(degreeArea);
+    reqBtn.class('degree-requirement');
+    reqBtn.attribute('requirementkey', requirement);
+
+    // div holding the courses for this requirement
+    let courseHolder = createDiv();
+    courseHolder.class('course-holder');
+    courseHolder.parent(degreeArea);
+    courseHolder.attribute('style', 'display: none');
+
+    // add description to requirement
+    this.createRequirementDescription(courseHolder);
+
+    // add each course to the requirement
+    for(let code in courses) {
+      
+    }
+
+    // make a horizontal bar to seperate each requirement
+    let hr = document.createElement('hr');
+    degreeArea.appendChild(hr);
+  }
+
+  createRequirementDescription(courseHolder) {
+
+  }
+}
